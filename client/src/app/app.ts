a3f4a6d9ab8d397932ff7b2181ff186b0dd63372
rootdobos <49080519+rootdobos@users.noreply.github.com>
2026-07-27 21:24:12 +0300

    Added Spartan

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HlmBadgeImports } from '@spartan-ng/helm/badge';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HlmBadgeImports],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('client');
 }
