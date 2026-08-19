c167306fe3371d2deb922457260e3dfd3d521517
rootdobos <49080519+rootdobos@users.noreply.github.com>
2026-08-19 22:03:53 +0300

    added click remove warning

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -2,9 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PlotLayoutComponent } from './layout/plot-layout/plot-layout.component';
 import { HeaderComponent } from "./layout/header/header.component";
+import { HlmToaster } from "@spartan-ng/helm/sonner";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, PlotLayoutComponent, HeaderComponent],
+  imports: [RouterOutlet, PlotLayoutComponent, HeaderComponent, HlmToaster],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, signal } from '@angular/core';
 import { HlmButtonImports } from '@spartan-ng/helm/button';
 import { NgIcon } from '@ng-icons/core';
+import { toast } from '@spartan-ng/brain/sonner';
 import { ThemeService } from '../../core/services/theme.service';
 import { HlmToggleImports } from '@spartan-ng/helm/toggle';
 import { HlmToasterImports } from '@spartan-ng/helm/sonner';
@@ -15,8 +16,26 @@ import { MapService } from '../../core/services/map.service';
 export class HeaderComponent {
   mapService = inject(MapService);
   readonly theme = inject(ThemeService);
-  onRemoveModeChanged(state:  'on' | 'off'): void {
+  onRemoveModeChanged(state: 'on' | 'off'): void {
     // pressed = true when active, false when deactivated
-    this.mapService.deletionEnabled.set(state === 'on')
+    this.mapService.deletionEnabled.set(state === 'on');
+    if (state === 'on') {
+      toast.warning('Click remove is on', {
+        description:
+          'If you click on an item, it will be removed from the map. It will remain in the inventory',
+        action: {
+          label: 'Close',
+          onClick: () => {},
+        },
+      });
+    } else {
+      toast.info('Click remove is off', {
+        description: 'You can safely click on the items',
+        action: {
+          label: 'Close',
+          onClick: () => {},
+        },
+      });
+    }
   }
 }
