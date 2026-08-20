d06d1ebf690f228010da8bbc17abeeed89d35961
rootdobos <49080519+rootdobos@users.noreply.github.com>
2026-08-20 14:19:24 +0300

    added postgres to backend

--- groundplotter.Api/Program.cs
+++ groundplotter.Api/Program.cs
@@ -1,8 +1,15 @@
+using Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -15,8 +22,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
